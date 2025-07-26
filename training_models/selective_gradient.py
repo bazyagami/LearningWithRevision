380df3684e49df70d2732ff9be2a52638d7ae8fb
bazyagami <shriram2010160@ssn.edu.in>
2025-07-26 19:53:56 +0000

    walltime

--- training_models/selective_gradient.py
+++ training_models/selective_gradient.py
@@ -449,6 +449,7 @@ class TrainRevision:
         print(f"\n✅ Total Wall Time for {self.epochs} epochs: {total_wall_time:.2f} seconds "
             f"({total_wall_time / 60:.2f} minutes)")
 
+
         plot_accuracy_time_multi(
         model_name=self.model_name,  
         accuracy=epoch_accuracies,
